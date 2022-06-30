04535bd0458edadf52d17c2b6e2cf56444d95cb0
Zack <zaxx81@gmail.com>
2022-06-30 15:18:22 -0400

    Improved algo for much larger nums...very fast!

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,3 +1,6 @@
+import math
+from re import I
+
 from time import time
   
 def timer_func(func):
@@ -18,10 +21,14 @@ def largest_prime_factor(n):
     
     while aggregate != n:
         temp = smallest_prime_factor(upper_factor)
-        if temp == 1:
-            break
+        if temp == upper_factor:
+            return upper_factor
         aggregate *= temp
         upper_factor = int(upper_factor / temp)
+        
+        # print(aggregate)
+        # print(f'Smallest Prime Factor: {temp}')
+        # print(f'Upper Factor: {upper_factor}')
     
     return upper_factor
 
@@ -49,15 +56,15 @@ def is_prime(n):
 
 # Finds the smallest prime factor of n
 def smallest_prime_factor(n):
-    if not n % 2:
+    if n % 2 == 0:
         return 2
 
-    end_range = int(n / 2) + 1
-
-    for i in range(3, end_range):
-        if is_prime(i) and is_factor(n, i):
+    i = 3
+    while(i * i <= n):
+        if n % i == 0:
             return i
-    return 1
+        i +=2
+    return n
 
 
 @timer_func
@@ -66,6 +73,24 @@ def test_case():
     # print(is_prime(1000000000100011)) # first 16-digit prime number
     # print(is_prime(10089886811898868001)) # first 20-digit prime number
     
-    print(largest_prime_factor(600851475143))
+    # print(smallest_prime_factor(473)) # => 11
+
+    
+    # print(smallest_prime_factor(3784)) # => 2
+    # print(smallest_prime_factor(1892)) # => 2
+    # print(smallest_prime_factor(946)) # => 2
+    # print(smallest_prime_factor(473)) # => 11
+    # print(smallest_prime_factor(43)) # => 43
+    # print(largest_prime_factor(3784)) # => 43
+    # print(largest_prime_factor(600851475143)) # => 6857
+    # print(largest_prime_factor(6008514751435)) # => 171671850041
+    # print(largest_prime_factor(25698751364526)) # => 328513
+
+    # print(largest_prime_factor(999999999999)) #12-digit arg is fast
+    # print(largest_prime_factor(9999999999999)) #13-digit arg
+    # print(largest_prime_factor(9999999999999)) #14-digit arg
+    # print(largest_prime_factor(9999999999999)) #15-digit arg
+    # print(largest_prime_factor(9999999999999)) #16-digit arg
+    print(largest_prime_factor(99999999999999999999)) #20-digit arg
 
 test_case()
